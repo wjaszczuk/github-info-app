dd4bc7785394e978eb65caa2e1662c5fdad8d319
Wiktor Jaszczuk <wiktor.jaszczuk@gmail.com>
2017-10-07 22:16:16 +0200

    add redux, reducers and actions

---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { header, title } from './header.css'
+
+export default () => (
+    <header className={header}>
+        <h1 className={title}>Github info app</h1>
+    </header>
+)
